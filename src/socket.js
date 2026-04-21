23dd7ea12acdcf443a770c16e485aa59fb626004
DiegoLizarraga <lizarragasanchezdiego@gmail.com>
2026-04-20 20:30:03 -0600

    jala aver

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,8 @@
 import { io } from 'socket.io-client';
 
-// prep for deployment
-const URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:3000';
+// Reemplaza 'TU_URL_DE_RAILWAY_AQUI' con el enlace público que te generó Railway. 
+// Ejemplo: 'https://mi-servidor-chattify.up.railway.app'
+const URL = import.meta.env.VITE_SOCKET_URL || 'chatify-server-production-9820.up.railway.app';
 
 export const socket = io(URL, {
   auth: {
